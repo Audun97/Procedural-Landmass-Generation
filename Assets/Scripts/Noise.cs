833d6dbba3b0aa79157aee744ba785891b34ab73
Audun97 <54212723+Audun97@users.noreply.github.com>
2019-10-14 22:02:33 +0200

    first

---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class will not be attached to any object and does therefore note extend MonoBehaviour
+//This class create no Instances and is therefore Static
+
+public static class Noise
+{
+    public static float [,] GenerateNoiseMap (int width, int height, float noiseScale)
+    {
+        float[,] noiseMap = new float[width, height];
+
+   
+    }
+}
